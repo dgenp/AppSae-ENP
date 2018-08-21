94f670e045ef27d09c787d6d4e707f5a43953936
Sistemas DGENP <sistemasdgenp@imac-de-sistemas.local>
2018-08-21 10:49:03 -0500

    Initial Commit

---
+++ AppSae ENPTests/AppSae_ENPTests.swift
@@ -0,0 +1,36 @@
+//
+//  AppSae_ENPTests.swift
+//  AppSae ENPTests
+//
+//  Created by Sistemas DGENP on 21/08/18.
+//  Copyright © 2018 Sistemas DGENP. All rights reserved.
+//
+
+import XCTest
+@testable import AppSae_ENP
+
+class AppSae_ENPTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
